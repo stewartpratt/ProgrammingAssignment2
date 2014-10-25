2995b63a9f6bf84aaefce863ca0332912f35eb0d
stewartpratt <stewart.a.pratt@gmail.com>
2014-10-25 16:42:19 -0400

    Programming Assignent 2
    Provides solution to cashing and calling the inverse of a matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
+## creates a special "matrix" object that can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
-
-}
+    inverse <<- NULL
+    set <- function(x) {
+        mat <<- x
+        inverse<<-NULL
+        #getinverse()
+    }
+    get <- function() {
+      mat
+    }    
+    getinverse <- function() {
+      inverse
+    }  
+    list( set = set, 
+          get = get,
+          getinverse=getinverse
+    )
+  }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+  ## Return a matrix that is the inverse of 'x'
+      i <- x$getinverse()
+      if(!is.null(i)) {
+        return(i)
+      }
+      mat <- x$get()
+      inverse <<- solve(mat)
+      inverse
+  }
